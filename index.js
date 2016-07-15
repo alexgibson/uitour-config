a70ca1bf4fd76ed692013f191030315025036db1
alexgibson <agibson@mozilla.com>
2016-07-15 16:02:49 +0100

    Renamed some functions to make things more readable.

--- index.js
+++ index.js
@@ -16,23 +16,67 @@ const UITOUR_ENABLED = 'browser.uitour.enabled';
 const REQUIRE_SECURE = 'browser.uitour.requireSecure';
 const TESTING_ORIGINS = 'browser.uitour.testingOrigins';
 
-const button = ToggleButton({
-    id: 'uitour-config',
-    label: 'Configure UITour',
-    icon: {
-        '16': './icon-16.png',
-        '32': './icon-32.png',
-        '64': './icon-64.png'
-    },
-    onChange: handleChange
-});
+let button;
 
+/**
+ * New profiles have no 'browser.uitour.testingOrigins' preference.
+ * Create one if it does not exist, using an empty value.
+ */
+function getTestingOrigins() {
+    if (!prefService.has(TESTING_ORIGINS)) {
+        prefService.set(TESTING_ORIGINS, '');
+    }
+    return prefService.get(TESTING_ORIGINS);
+}
+
+/**
+ * Add-on ToggleButton
+ */
+function setToggleButton() {
+    button = ToggleButton({
+        id: 'uitour-config',
+        label: 'Configure UITour',
+        icon: {
+            '16': './icon-16.png',
+            '32': './icon-32.png',
+            '64': './icon-64.png'
+        },
+        onChange: handleToggleButtonChange
+    });
+}
+
+function handleToggleButtonChange(state) {
+    if (state.checked) {
+        configPanel.show({
+            position: button
+        });
+    }
+}
+
+setToggleButton();
+
+/**
+ * Add-on UI Panel
+ */
 const configPanel = Panel({
     contentURL: self.data.url('panel.html'),
     contentScriptFile: self.data.url('panel.js'),
-    onShow: handleShow,
-    onHide: handleHide
+    onShow: handleConfigPanelShow,
+    onHide: handleConfigPanelHide
 });
+
+// when the panel is shown show the current values
+function handleConfigPanelShow() {
+    configPanel.port.emit('set-uitour-enabled', prefService.get(UITOUR_ENABLED));
+    configPanel.port.emit('set-require-secure', prefService.get(REQUIRE_SECURE));
+}
+
+// when the panel is hidden make sure the button state is also unchecked.
+function handleConfigPanelHide() {
+    button.state('window', {
+        checked: false
+    });
+}
 
 configPanel.port.on('toggle-uitour-enabled', (state) => {
     prefService.set(UITOUR_ENABLED, state);
@@ -49,33 +93,3 @@ configPanel.port.on('add-to-whitelist', () => {
         prefService.set(TESTING_ORIGINS, newOrigins);
     }
 });
-
-function handleChange(state) {
-    if (state.checked) {
-        configPanel.show({
-            position: button
-        });
-    }
-}
-
-function handleShow() {
-    configPanel.port.emit('set-uitour-enabled', prefService.get(UITOUR_ENABLED));
-    configPanel.port.emit('set-require-secure', prefService.get(REQUIRE_SECURE));
-}
-
-function handleHide() {
-    button.state('window', {
-        checked: false
-    });
-}
-
-/**
- * New profiles have no 'browser.uitour.testingOrigins' preference.
- * Create one if it does not exist, using an empty value.
- */
-function getTestingOrigins() {
-    if (!prefService.has(TESTING_ORIGINS)) {
-        prefService.set(TESTING_ORIGINS, '');
-    }
-    return prefService.get(TESTING_ORIGINS);
-}
