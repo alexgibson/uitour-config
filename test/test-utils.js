a0d186a9853c6440d661355f6930118f5c6c9085
alexgibson <agibson@mozilla.com>
2016-07-21 11:36:50 +0100

    Tidy up verbose test output

--- test/test-utils.js
+++ test/test-utils.js
@@ -10,13 +10,16 @@ const testRunner = require('sdk/test');
 
 exports.testGetHostURL = (assert) => {
     let url = 'https://www.allizom.org/en-US/firefox/new/';
-    assert.strictEqual(utils.getHostURL(url), 'https://www.allizom.org');
+    assert.strictEqual(utils.getHostURL(url), 'https://www.allizom.org',
+        'Host value returned as expected');
 
     url = 'http://127.0.0.8000/en-US/';
-    assert.strictEqual(utils.getHostURL(url), 'http://127.0.0.8000');
+    assert.strictEqual(utils.getHostURL(url), 'http://127.0.0.8000',
+        'Host value returned as expected');
 
     url = 'about:home';
-    assert.strictEqual(utils.getHostURL(url), null);
+    assert.strictEqual(utils.getHostURL(url), null,
+        'Invalid URL should return null');
 };
 
 exports.testAddToCommaString = (assert) => {
@@ -24,16 +27,16 @@ exports.testAddToCommaString = (assert) => {
 
     assert.strictEqual(utils.addToCommaString('https://www-demo1.allizom.org', commaString),
         'https://www-demo1.allizom.org',
-        'Single items should not have a trailing comma');
+        'Single values should not have a trailing comma');
 
     commaString = 'https://www.allizom.org';
     assert.strictEqual(utils.addToCommaString('https://www-demo1.allizom.org', commaString),
         'https://www.allizom.org,https://www-demo1.allizom.org',
-        'Additional items should get added');
+        'Additional values should get added');
 
     commaString = 'https://www.allizom.org,https://www-demo1.allizom.org';
     assert.strictEqual(utils.addToCommaString('https://www-demo1.allizom.org', commaString),
-        commaString, 'Duplicate items should not get added');
+        commaString, 'Duplicate values should not get added');
 
     commaString = 'https://www.allizom.org,https://www-demo1.allizom.org';
     assert.strictEqual(utils.addToCommaString(null, commaString), commaString,
@@ -41,9 +44,12 @@ exports.testAddToCommaString = (assert) => {
 };
 
 exports.testArrayFromCommaString = (assert) => {
-    assert.strictEqual(utils.arrayFromCommaString('').length, 0);
-    assert.strictEqual(utils.arrayFromCommaString('https://www.allizom.org').length, 1);
-    assert.strictEqual(utils.arrayFromCommaString('https://www-demo1.allizom.org,https://www.allizom.org').length, 2);
+    assert.strictEqual(utils.arrayFromCommaString('').length, 0,
+        'Array length is as expected');
+    assert.strictEqual(utils.arrayFromCommaString('https://www.allizom.org').length, 1,
+        'Array length is as expected');
+    assert.strictEqual(utils.arrayFromCommaString('https://www-demo1.allizom.org,https://www.allizom.org').length, 2,
+        'Array length is as expected');
 };
 
 testRunner.run(exports);
